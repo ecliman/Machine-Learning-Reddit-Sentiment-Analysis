42388d3d713f2da395966916e00e6cad86fdbd3b
alexywang <alex.yuanda.wang@gmail.com>
2019-10-16 23:58:33 -0400

    Getting NB classifier to work witih sparse matrices

--- root/models/nb_classifier.py
+++ root/models/nb_classifier.py
@@ -1,37 +1,62 @@
 import numpy as np
+from scipy import sparse
 
-class NaiveBayes:
-    l_funcs: {} # likelihood functions
-    x_train = None
-    y_train = None
-    x_split = {}
-    class_probs = None
+class SparseNaiveBayes:
 
-    # Split x into separate ndarrays based on their class
-    def __split_x(self, x_train, y_train):
+    def __init__(self):
+        return
+
+    # Split x into separate sparse matricies based on their class
+    @staticmethod
+    def split_x(x_train, y_train):
         x_split = dict()
-        for row in range(0, x_train.shape[0]):
-            key = y_train[row]
-            if key in x_split:
-                x_split[key].append(x_train[row])
-            else:
-                x_split[key] = [x_train[row]]
-        for key in x_split:
-            x_split[key] = np.asarray(x_split[key])
-
-        self.x_split = x_split
+        # Iterate example by example
+        for i in range(0, len(y_train)):
+            cls = y_train[i]
+            example = x_train[i]
+            if cls in x_split:  # Stack onto existing split
+                x_split[cls] = sparse.vstack([x_split[cls], example])
+            else:  # Create new key for split
+                x_split[cls] = sparse.csr_matrix(example)
+        return x_split
 
     @staticmethod
-    def __cond_prob(x_split, class_val):
-        probs = list()
-        split = x_split[class_val]
-        for col in range(0, split.shape[1]):
-            count = 0
-            for row in range(0, split.shape[0]):
-                if split[row][col] > 0:
-                    count += 1
-            probs.append(count)
-        for p in range(0, len(probs)):
-            probs[p] = probs[p] / split.shape[0]
-        return probs
+    def get_class_probs(x_train, x_split):
+        class_probs = dict()
+        for cls in x_split:
+            class_probs[cls] = x_split[cls].shape[0]/x_train.shape[0]
+        return class_probs
 
+    @staticmethod
+    def get_thetas(x_split):
+        thetas = dict()
+        # For each feature in each class
+        for cls in x_split:
+            cur_split = x_split[cls]
+            non_zero = cur_split.nonzero()[1]
+            # Count occurences of non-zeros in columns
+            unique, counts = np.unique(non_zero, return_counts=True)
+            occurrences = dict(zip(unique, counts))
+            thetas[cls] = [0] * cur_split.shape[1]
+            for key in occurrences:
+                thetas[cls][key] = occurrences[key]
+
+        return thetas
+
+
+# Testing
+model = SparseNaiveBayes()
+x = sparse.load_npz('../data/xtrainbin.npz')
+y = np.load('../data/y_train.npy')
+
+
+# Splitting
+split = model.split_x(x, y)
+
+# Class Probs
+class_probs = model.get_class_probs(x, split)
+print(class_probs)
+
+# Thetas
+thetas = model.get_thetas(split)
+print(type(thetas))
